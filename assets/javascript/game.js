8e1f947dc69abee93e7f5d4f3b70c809f72a6207
Christopher Mangin <mangin.christopher@gmail.com>
2019-10-07 20:50:00 -0400

    fixed bug where computer kept generating new letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,54 @@
-
-
-    var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", 
+"i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", 
+"t", "u", "v", "w", "x", "y", "z"];
 
 		 var wins = 0;
 		 var losses = 0;
-		 var guessCount = 9;
-		 var guessChoices = [""];
+		 var remaining = 9;
+		 var guesses = 9
+		 var guessesSoFar = [""];
+		 var computerLetter;
 
-		 document.onkeyup = function(event) {
-
-		var userGuess = event.key;
-		var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-		var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-
-			if (letters.indexOf(userGuess) > -1) {
-
-				if (userGuess === computerGuess) {
-					wins++;
-					guessCount = 9;
-					guessChoices = [];
-				}
-
-				if (userGuess != computerGuess) {
-					guessCount --;
-					guessChoices.push(userGuess);
-				}
-
-				if (guessCount === 0) {
-
-				guessCount = 9;
-				losses ++;
-				guessChoices = [];
-
-			}
-
-	var html = 
-	"<h1>**The Psychic Game**</h1>" +
-	"<p>Guess what letter I'm thinking of!</p>" +
-	"<p>Wins: " + wins + "</p>" +
-	"<p>Losses: " + losses + "</p>" +
-	"<p>Guesses Left: " + guessCount + "</p>" +
-	"<p>Your Guesses So Far: " + guessChoices.join(", ") + "</p>";
-
-		document.querySelector("#game").innerHTML = html;
-
-			}
+		 var newLetter = function() {
+			computerLetter = letters[Math.floor(Math.random() * letters.length)];
+		};
+		
+		var soFar = function() {
+			document.getElementById("guesses").innerHTML = "Guesses: " + guessesSoFar;
+		};
+		
+		var guessesremaining = function() {
+			document.getElementById("remaining").innerHTML = "Guesses Remaining: " + remaining;
+		};
+		
+		var newGame = function() {
+			guessedLetters = [];
+			remaining = 9;
+			newLetter();
+			guessesremaining();
+			soFar();
 		}
+		
+		document.onkeyup = function(event) {
+
+			var userGuess = event.key;
+			remaining--;
+			guessesSoFar.push(userGuess);
+			soFar();
+			guessesremaining();
+
+			if (remaining > 0) {
+
+				if (userGuess === computerLetter) {
+					wins++;
+					document.getElementById("wins").innerHTML = "Wins: " + wins;
+					newGame();
+					guessesSoFar = [];}
+
+						} else if (remaining === 0) {
+							losses++;
+							document.getElementById("losses").innerHTML = "Losses: " + losses;
+							newGame();
+							guessesSoFar = [];
+						}
+		};
